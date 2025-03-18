55c5e27036e8b1b45148be8cce0011235d975bb3
Student <you@example.com>
2025-03-18 08:50:03 +0300

    js-code

---
+++ string.js
@@ -0,0 +1,9 @@
+let words ="extravaganza";
+let lastfour=words.slice(4);
+console.log(lastfour);
+
+costfood='words';
+constnewfood.slice(0,4) +'eat'+ food.slice(4);
+console.log(newfood);
+conststory="The quick brown fox jump over the lazy dog";
+let count=Storage.tolowercase().split('the').length-1
